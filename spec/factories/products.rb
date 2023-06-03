61f51574f914dd587984e6524f32b90da3988f9d
João Vitor <joaovitorlimap@gmail.com>
2023-06-03 17:20:33 -0300

    018[config]: add seeds

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :product do
-    name { Faker::Commerce.product_name }
+    name { Faker::Commerce.unique.product_name }
     start_date { Faker::Date.between(from: Time.zone.today, to: '2024-12-31') }
     final_date { Faker::Date.between(from: Date.tomorrow, to: '2024-12-31') }
     minimum_amount { Faker::Number.decimal(l_digits: 4, r_digits: 2) }
--- spec/factories/taxes.rb
+++ spec/factories/taxes.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :tax do
-    name { Faker::Ancient.god }
+    name { Faker::Ancient.unique.god }
     value { Faker::Number.decimal(l_digits: 2, r_digits: 2) }
   end
 end
