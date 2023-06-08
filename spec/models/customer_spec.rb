3d64044bca385720f5aaf564a1fb14eb8994ebab
Jadson Rubens Rocha de Oliveira <jadson_126@hotmail.com>
2023-06-08 00:43:20 -0300

    026 [feat]: add test and rubocop

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -8,4 +8,10 @@ RSpec.describe Customer, type: :model do
     it { should have_one(:user).class_name('User') }
     it { should have_one(:contact_list).class_name('ContactList') }
   end
+  describe '#full_name' do
+    it 'returns the full name with initial' do
+      customer = Customer.new(first_name: 'John', last_name: 'Doe')
+      expect(customer.full_name).to eq('D. John')
+    end
+  end
 end
