03c1089676bcbf3eda8ca4cdc4191e445d659300
XitaRps <xitarps@gmail.com>
2023-06-12 05:41:09 -0300

    025 [Refac]: Adjust transfer presentation

--- app/controllers/transfers_controller.rb
+++ app/controllers/transfers_controller.rb
@@ -10,13 +10,18 @@ class TransfersController < ApplicationController
   def show; end
 
   def new
+    @receiver_cpf = params[:cpf] if params.fetch(:cpf, nil)
     @transfer = current_user.userable&.account&.transfers.new
   end
 
   def create
     @transfer = Transfer.new(transfer_params.merge({ sender_id: current_user.userable&.account.id }))
 
-    return redirect_to @transfer if @transfer.save
+    if @transfer.save
+      persist_contact
+      return redirect_to @transfer
+
+    end
 
     render :new
   end
@@ -24,11 +29,27 @@ class TransfersController < ApplicationController
   private
 
   def transfer_params
-    params.require(:transfer).permit(:receiver_id, :amount)
+    @raw_params = params.require(:transfer).permit(:receiver_cpf, :amount)
+    handle_cpf
   end
 
   def search_transfer
     @transfer = current_user.userable&.account&.transfers.find(params[:id]) if current_user.userable_type == 'Customer'
     @transfer ||= Transfer.find(params[:id])
+  end
+
+  def handle_cpf
+    if @raw_params.fetch(:receiver_cpf, nil)
+      @receiver = Account.joins(:customer).where(customer: { cpf: @raw_params[:receiver_cpf] } )&.first
+      @raw_params.tap do |param|
+        param[:receiver_id] = @receiver&.id
+        param.delete(:receiver_cpf)
+      end
+    end
+  end
+
+  def persist_contact
+    save_contact = params[:transfer].fetch(:save_contact, nil)
+    current_user.userable&.contact_list&.customers << @receiver&.customer if !save_contact&.to_i&.zero? && @receiver != current_user&.userable&.account
   end
 end
